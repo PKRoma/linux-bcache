990f9f55653c1149859b1b5b3a4b6f0f6b8232ce
Kent Overstreet <kent.overstreet@gmail.com>
2015-03-29 19:59:29 -0700

    bcache: Set cur_pin_list correctly when journal replay done
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1113,7 +1113,6 @@ static const char *run_cache_set(struct cache_set *c)
 			}
 
 		bch_journal_replay(c, &journal);
-		set_bit(JOURNAL_REPLAY_DONE, &c->journal.flags);
 	} else {
 		pr_notice("invalidating existing data");
 
@@ -1150,7 +1149,7 @@ static const char *run_cache_set(struct cache_set *c)
 		 * written until the SET_CACHE_SYNC() here:
 		 */
 		SET_CACHE_SYNC(&c->sb, true);
-		set_bit(JOURNAL_REPLAY_DONE, &c->journal.flags);
+		bch_journal_set_replay_done(&c->journal);
 
 		bch_journal_meta(c, &cl);
 	}
