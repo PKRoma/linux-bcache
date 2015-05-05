21d8a19ed45d2ba96a38d6f982e609dc99d4f80b
Kent Overstreet <kent.overstreet@gmail.com>
2015-05-05 15:10:05 -0700

    bcache: closure stuff

--- drivers/md/bcache/closure.h
+++ drivers/md/bcache/closure.h
@@ -343,6 +343,12 @@ do {									\
 	return;								\
 } while (0)
 
+#define closure_return_with_destructor_noreturn(_cl, _destructor)	\
+do {									\
+	set_closure_fn(_cl, _destructor, NULL);				\
+	closure_sub(_cl, CLOSURE_RUNNING - CLOSURE_DESTRUCTOR + 1);	\
+} while (0)
+
 /**
  * closure_return - finish execution of a closure, with destructor
  *
@@ -354,8 +360,7 @@ do {									\
  */
 #define closure_return_with_destructor(_cl, _destructor)		\
 do {									\
-	set_closure_fn(_cl, _destructor, NULL);				\
-	closure_sub(_cl, CLOSURE_RUNNING - CLOSURE_DESTRUCTOR + 1);	\
+	closure_return_with_destructor_noreturn(_cl, _destructor);	\
 	return;								\
 } while (0)
 
