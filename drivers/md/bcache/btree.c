95df8efc74330552663656d083b0fccf1f39cf87
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-21 00:33:17 -0800

    bcache: turn gc_mark_valid into a rw sem
    Also reduce bucket_lock usage
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- drivers/md/bcache/btree.c
+++ drivers/md/bcache/btree.c
@@ -1957,16 +1957,12 @@ static void btree_gc_start(struct cache_set *c)
 	struct bucket *g;
 	unsigned i;
 
-	/* We should not be doing a GC while trying to start GC */
-	BUG_ON(!c->gc_mark_valid);
-
 	mutex_lock(&c->bucket_lock);
 
 	write_seqlock(&c->gc_cur_lock);
 	for_each_cache(ca, c, i)
 		ca->bucket_stats_cached = __bucket_stats_read(ca);
 
-	c->gc_mark_valid = 0;
 	c->gc_cur_btree = 0;
 	c->gc_cur_key = ZERO_KEY;
 	write_sequnlock(&c->gc_cur_lock);
@@ -1998,10 +1994,6 @@ static void bch_btree_gc_finish(struct cache_set *c)
 
 	set_gc_sectors(c);
 
-	write_seqlock(&c->gc_cur_lock);
-	c->gc_mark_valid = 1;
-	write_sequnlock(&c->gc_cur_lock);
-
 	bch_mark_writeback_keys(c);
 	bch_mark_keybuf_keys(c, &c->tiering_keys);
 
@@ -2021,6 +2013,10 @@ static void bch_btree_gc_finish(struct cache_set *c)
 	}
 
 	mutex_unlock(&c->bucket_lock);
+
+	write_seqlock(&c->gc_cur_lock);
+	c->gc_cur_btree = BTREE_ID_NR + 1;
+	write_sequnlock(&c->gc_cur_lock);
 }
 
 /**
@@ -2041,6 +2037,7 @@ static void bch_btree_gc(struct cache_set *c)
 
 	memset(&stats, 0, sizeof(struct gc_stat));
 
+	down_write(&c->gc_lock);
 	btree_gc_start(c);
 
 	while (c->gc_cur_btree < BTREE_ID_NR) {
@@ -2068,6 +2065,7 @@ static void bch_btree_gc(struct cache_set *c)
 	}
 
 	bch_btree_gc_finish(c);
+	up_write(&c->gc_lock);
 
 	bch_time_stats_update(&c->btree_gc_time, start_time);
 
