51c9d4e01e308b41701999fb876f907c30cde3cd
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-21 01:42:20 -0800

    bcache: Expose new user_uuid instead of internal cache set uuid
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/trace/events/bcache.h
+++ include/trace/events/bcache.h
@@ -164,7 +164,7 @@ TRACE_EVENT(bcache_write,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->inode		= inode;
 		__entry->sector		= bio->bi_iter.bi_sector;
 		__entry->nr_sector	= bio->bi_iter.bi_size >> 9;
@@ -194,7 +194,7 @@ TRACE_EVENT(bcache_write_throttle,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->inode		= inode;
 		__entry->sector		= bio->bi_iter.bi_sector;
 		__entry->nr_sector	= bio->bi_iter.bi_size >> 9;
@@ -229,7 +229,7 @@ DECLARE_EVENT_CLASS(page_alloc_fail,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->size = size;
 	),
 
@@ -247,7 +247,7 @@ DECLARE_EVENT_CLASS(cache_set,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 	),
 
 	TP_printk("%pU", __entry->uuid)
@@ -268,7 +268,7 @@ TRACE_EVENT(bcache_journal_write_oldest,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->seq		= seq;
 	),
 
@@ -286,7 +286,7 @@ TRACE_EVENT(bcache_journal_write_oldest_done,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->seq		= seq;
 		__entry->written	= written;
 	),
@@ -380,7 +380,7 @@ DECLARE_EVENT_CLASS(btree_node,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, b->c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, b->c->sb.user_uuid.b, 16);
 		__entry->bucket		= PTR_BUCKET_NR_TRACE(b->c, &b->key, 0);
 		__entry->level		= b->level;
 		__entry->id		= b->btree_id;
@@ -438,7 +438,7 @@ TRACE_EVENT(bcache_btree_node_alloc_fail,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->id = id;
 	),
 
@@ -481,7 +481,7 @@ TRACE_EVENT(bcache_mca_scan,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->touched	= touched;
 		__entry->freed		= freed;
 		__entry->can_free	= can_free;
@@ -503,7 +503,7 @@ DECLARE_EVENT_CLASS(mca_cannibalize_lock,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->cl = cl;
 	),
 
@@ -543,7 +543,7 @@ DECLARE_EVENT_CLASS(btree_node_op,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, b->c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, b->c->sb.user_uuid.b, 16);
 		__entry->bucket	= PTR_BUCKET_NR_TRACE(b->c, &b->key, 0);
 		__entry->level	= b->level;
 		__entry->id	= b->btree_id;
@@ -705,7 +705,7 @@ TRACE_EVENT(bcache_btree_node_alloc_replacement,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, b->c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, b->c->sb.user_uuid.b, 16);
 		__entry->old_bucket	= PTR_BUCKET_NR_TRACE(old->c,
 							      &old->key, 0);
 		__entry->bucket		= PTR_BUCKET_NR_TRACE(b->c, &b->key, 0);
@@ -923,7 +923,7 @@ DECLARE_EVENT_CLASS(cache_set_bucket_alloc,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->reserve = reserve;
 		__entry->cl = cl;
 	),
@@ -954,7 +954,7 @@ DECLARE_EVENT_CLASS(open_bucket_alloc,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->cl = cl;
 	),
 
@@ -1177,7 +1177,7 @@ TRACE_EVENT(bcache_tiering_end,
 	),
 
 	TP_fast_assign(
-		memcpy(__entry->uuid, c->sb.set_uuid.b, 16);
+		memcpy(__entry->uuid, c->sb.user_uuid.b, 16);
 		__entry->sectors_moved = sectors_moved;
 		__entry->keys_moved = keys_moved;
 	),
